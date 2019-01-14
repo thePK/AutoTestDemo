1f77e7b71ed476be613ae78fad3678894ac7c987
KePeng <KePeng@163.com>
2019-01-14 20:35:33 +0800

    the third commit to test the login function of hhumis,casue the Cmis is denied;

--- src/test/java/com/pengke/AutoTestDemo/TestLogin.java
+++ src/test/java/com/pengke/AutoTestDemo/TestLogin.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class TestLogin {
 	WebDriver driver;
   @Test
-  public void testLogin() {
+  public void testCMISLogin() {
 	   driver.get("http://p101023.jspdi.com.cn:8080/x5/UI2/v_/portal/pc3/adminLogin.w#!main");
 	   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	   WebElement webElement1 = driver.findElement(By.xpath("//*[@placeholder='用户名']"));
@@ -21,6 +21,39 @@ public class TestLogin {
 	   webElement2.sendKeys("cmiS");
 	   driver.findElement(By.xpath("//*[@xid='login']")).click();
   }
+  
+  @Test
+  public void testHHULogin() {
+		driver.get("http://gs.hhu.edu.cn/");
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//table[@id='quicklink']/tbody/tr/td[3]/a")).click();
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String handle = driver.getWindowHandle();
+		for(String handles : driver.getWindowHandles()){
+			if(handles.equals(handle))
+				continue;
+				//driver.close();
+			driver.switchTo().window(handles);
+		}
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		
+		driver.findElement(By.xpath("//div[@class='login']//input[@name='username']")).click();
+		
+		driver.findElement(By.xpath("//div[@class='login']//input[@name='username']")).sendKeys("171607010018");
+		
+		driver.findElement(By.xpath("//div[@class='login']//input[@name='password']")).click();
+		
+		driver.findElement(By.xpath("//div[@class='login']//input[@name='password']")).sendKeys("LPwAN9511243461");
+		
+		driver.findElement(By.id("login")).click();
+		
+	}
+  
   
   @BeforeClass
   public void beforeClass() {
